273868fb39f42ebe3d27f547d93ec6ade18c089e
DESKTOP-955G2G9\ccc <qma@cccis.com>
2017-11-03 17:35:45 +0800

    Add async cloud case.

---
+++ async-cloud/src/test/java/com/coral/async/test/AsyncApplicationTest.java
@@ -0,0 +1,45 @@
+package com.coral.async.test;
+
+import com.coral.Application;
+import com.coral.async.AsyncTask;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.concurrent.Future;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = Application.class)
+public class AsyncApplicationTest {
+
+
+    @Autowired
+    private AsyncTask task;
+
+    @Test
+    public void test() throws Exception {
+
+        long start = System.currentTimeMillis();
+
+        Future<String> task1 = task.doTaskOne();
+        Future<String> task2 = task.doTaskTwo();
+        Future<String> task3 = task.doTaskThree();
+
+        while(true) {
+            if(task1.isDone() && task2.isDone() && task3.isDone()) {
+                // 三个任务都调用完成，退出循环等待
+                break;
+            }
+            Thread.sleep(1000);
+        }
+
+        long end = System.currentTimeMillis();
+
+        System.out.println("任务全部完成，总耗时：" + (end - start) + "毫秒");
+
+    }
+
+}
